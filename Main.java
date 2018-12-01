b5e45e397b7b8ca7ef21685bb27b61cbfe85dec8
BeeBeeBree <44854515+BeeBeeBree@users.noreply.github.com>
2018-12-01 15:13:08 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,7 +2,11 @@
 public class Main {
 
 	public static void main(String[] args) {
-		Snake snake = new Snake(0, 0, 1, 0);
+		Snake snake = new Snake(4, 0, 1, 0);
+		SnakePart P1 = new SnakePart(3,0,0,0,snake);
+		SnakePart P2 = new SnakePart(2,0,0,0, snake);
+		SnakePart P3 = new SnakePart(1,0,0,0, snake);
+		SnakePart P4 = new SnakePart(0,0,0,0, snake);
 		//new BoardGraphics(snake, 50);
 		new BoardGraphics();
 	}
